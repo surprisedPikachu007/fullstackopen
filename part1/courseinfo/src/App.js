30e650c11623ddda978d28da98bb2a6b88dfc03a
surprisedPikachu69 <arvind7447@gmail.com>
2023-06-20 11:56:02 +0530

    part1 - Ex 1.1 to 1.2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,51 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.exercise} {props.part}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part exercise={props.exercises1} part={props.part1}></Part>
+      <Part exercise={props.exercises2} part={props.part2}></Part>
+      <Part exercise={props.exercises3} part={props.part3}></Part>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>{props.total}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}></Content>
+      <Total total={exercises1 + exercises2 + exercises3}></Total>
+    </div>
+  )
+}
+
+export default App
